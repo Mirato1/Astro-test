3e1aa97ace602c86ae44bbfc257f08e615dd5785
Lucas Nahuel Giménez <luquetti.5@hotmail.com>
2023-09-04 23:04:54 -0300

    💥 NEW: Vercel config to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,17 @@ import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		react({
-			include: ['**/react/*'],
-		}),
-	],
-	image: {
-		service: squooshImageService(),
-		domains: ['astro.build', 'https://raw.githubusercontent.com/'],
-	},
-	output: 'server',
+  integrations: [tailwind(), react({
+    include: ['**/react/*']
+  })],
+  image: {
+    service: squooshImageService(),
+    domains: ['astro.build', 'https://raw.githubusercontent.com/']
+  },
+  output: 'server',
+  adapter: vercel()
 });
