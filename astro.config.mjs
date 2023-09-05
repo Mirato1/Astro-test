51a3346e0dced58c54fedc153b1a3f46183a6325
Lucas Nahuel Giménez <luquetti.5@hotmail.com>
2023-09-04 23:26:51 -0300

    🐛 FIX: Imports errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react({
-    include: ['**/react/*']
-  })],
-  image: {
-    service: squooshImageService(),
-    domains: ['astro.build', 'https://raw.githubusercontent.com/']
-  },
-  output: 'server',
-  adapter: vercel()
+	integrations: [
+		tailwind(),
+		react({
+			include: ['**/react/*'],
+		}),
+	],
+	image: {
+		service: squooshImageService(),
+		domains: ['astro.build', 'https://raw.githubusercontent.com/'],
+	},
+	output: 'server',
+	adapter: vercel(),
 });
